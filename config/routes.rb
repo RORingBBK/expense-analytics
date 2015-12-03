cb5e48b26c147da5f04ac326ae5f76bf3bad3d2f
RORingBBK <bibek.khadka@nbinfotech.com.np>
2015-12-03 17:11:17 +0545

    head resolved

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-<<<<<<< HEAD
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
-=======
-
   get 'static_pages/help'
 
   get 'static_pages/about'
->>>>>>> temp
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
