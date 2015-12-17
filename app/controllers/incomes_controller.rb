b9b381362134ecb9f25a7221b9f97cfaf308e83d
RORingBBK <bibek.khadka@nbinfotech.com.np>
2015-12-17 17:29:20 +0545

    Income on Progress

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -1,16 +1,26 @@
 class IncomesController < ApplicationController
 
 	def index
-		@incomes = Income.all
+		all_income
+		@income = Income.find(params[:id])
+		# @incomes = Income.paginate(page: params[:page], per_page: 5)
+		# @incomes = Income.where(member_id: current_member.id)
 	end
 
 	def show
-		@income = Income.find(params[:id])
-		render :template => 'incomes/index'
+		all_income
+		# @incomes = Income.paginate(page => params[:page], :per_page => 30)
+		redirect_to new_income_path
 	end
 
 	def new
 		@income = Income.new
+		all_income
+		@incomes = Income.paginate(page: params[:page], per_page: 5)
+	end
+
+	def edit
+		@income = Income.find(params[:id])
 	end
 
 	def create
@@ -23,9 +33,31 @@ class IncomesController < ApplicationController
 		end
 	end
 
+	def update
+		@income = Income.find(params[:id])
+		if @income.update(income_params)
+			flash[:success] = "Income updated."
+			redirect_to @income
+		else
+			render 'edit'
+		end
+end
+
+	def destroy
+		@income = Income.find(params[:id])
+		@income.destroy
+
+		redirect_to new_income_path
+	end
+
 	private
+
 		def income_params
 			params.require(:income).permit(:title, :description, :amount, :date, :member_id)
+		end
+
+		def all_income
+			@incomes = Income.all
 		end
 
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,13 @@ module ApplicationHelper
 		end
 	end
 
+	# will_paginate gem setup.
+	# def pagination_links(collection, options={})
+	# 	options[:renderer] ||= BootstrapPaginationHelper::LinkRenderer
+	# 	options[:class] ||= 'pagination pagination-centered'
+	# 	options[:inner_window] ||= 2
+	# 	options[:outer_window] ||= 1
+	# 	will_paginate(collection, options)
+	# end
+
 end
