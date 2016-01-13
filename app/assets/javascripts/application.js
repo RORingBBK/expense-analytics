bfcbefa82a9eb42a6a4c762cebd2b84b6ff13647
RORingBBK <bibek.khadka@nbinfotech.com.np>
2016-01-13 13:34:07 +0545

    Print chart

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -31,5 +31,18 @@ $(document).ready(function(){
 	}, 4500);
 });
 
+// For printing charts
+// function printpage() {
+// 	window.print()
+// }
+function printDiv(divName) {
+	var printContents = document.getElementById(divName).innerHTML;
+	var originalContents = document.body.innerHTML;
+
+	document.body.innerHTML = printContents;
+
+	window.print();
+	document.body.innerHTML = originalContents;
+}
 
 
