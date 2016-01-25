70687a2af1dd90c76f1cb3f8fca96691f75f9158
RORingBBK <bibek.khadka@nbinfotech.com.np>
2016-01-25 14:47:40 +0545

    Final commit from git.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160106075447) do
+ActiveRecord::Schema.define(version: 20160125072028) do
 
   create_table "categories", force: :cascade do |t|
     t.text     "title"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20160106075447) do
     t.integer  "member_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.text     "category"
   end
 
   add_index "expenses", ["member_id"], name: "index_expenses_on_member_id"
@@ -72,12 +73,12 @@ ActiveRecord::Schema.define(version: 20160106075447) do
 
   create_table "reminders", force: :cascade do |t|
     t.text     "title"
-    t.integer  "amount"
-    t.text     "description"
     t.date     "date_notify"
-    t.integer  "member_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "amount"
+    t.text     "description"
+    t.integer  "member_id"
   end
 
   add_index "reminders", ["member_id"], name: "index_reminders_on_member_id"
